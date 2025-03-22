cb5eccf51344833251e624c44d0a5b0e4ea0ab14
Pedro Soares <pedrohenriquegago2@gmail.com>
2025-03-22 09:58:39 -0300

    aula3 consertado

--- code/EntityFactoring.py
+++ code/EntityFactoring.py
@@ -10,6 +10,6 @@ class EntityFactory:
             case 'Level1Bg':
                 list_bg = []
                 for i in range(3):
-                    list_bg.append(Background(f'Level1Bg{i}', position(0, 0)))
-                    list_bg.append(Background(f'Level1Bg{i}', position(WIN_WIDTH, 0)))
+                    list_bg.append(Background(f'Level1Bg{i}', (0, 0)))
+                    list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH, 0)))
                 return list_bg
