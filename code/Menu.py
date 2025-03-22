b812841a1b9d02146e39602847769d624c86bae2
Pedro Soares <pedrohenriquegago2@gmail.com>
2025-03-22 16:32:19 -0300

    aula 4 parte1 12min

--- code/Menu.py
+++ code/Menu.py
@@ -8,7 +8,7 @@ from code.Const import WIN_WIDTH, WIN_HEIGHT, C_BLUEF, MENU_OPTION, C_BLUE, C_WH
 class Menu:
     def __init__(self, window):
         self.window = window
-        self.surf = pygame.image.load('./asset/backgroundmenu.png')
+        self.surf = pygame.image.load('./asset/backgroundmenu.png').convert_alpha()
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
