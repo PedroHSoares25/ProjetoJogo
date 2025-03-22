d8ea91a7ce48e6191114ca686fcd3721bd6efa83
Pedro Soares <pedrohenriquegago2@gmail.com>
2025-03-22 09:59:10 -0300

    aula3 consertado

---
+++ code/Const.py
@@ -0,0 +1,19 @@
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
+
+C_BLUEF = (10, 37, 89)
+C_BLUE = (3, 10, 59)
+C_WHITE = (255, 255, 255)
+
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT')
+
+ENTITY_SPEED = {
+    'Level1Bg0' : 0,
+    'Level1Bg1' : 1,
+    'Level1Bg2' : 2,
+    'Level1Bg3' : 3,
+}
